99cf3cd1b5c5d2b6e4d79981a5523d0a7ee2e64c
JoshAlexis <joshyelc@gmail.com>
2022-10-08 17:16:20 -0600

    feat: Setting Prisma Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { PrismaModule } from './prisma/presentation/prisma.module'
+import { PrismaService } from './prisma/infrastructure/prisma.service'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [PrismaModule],
+	controllers: [AppController],
+	providers: [AppService, PrismaService]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import helmet from 'helmet'
+import { ValidationPipe, VersioningType } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule)
+
+	app.use(helmet())
+	app.enableCors()
+	app.setGlobalPrefix('api')
+	app.enableVersioning({
+		type: VersioningType.URI
+	})
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+			forbidUnknownValues: true,
+			validationError: {
+				value: false
+			}
+		})
+	)
+
+	await app.listen(3000)
 }
-bootstrap();
+bootstrap()
