e2abe7129665fa9076cd696e37c4dd841298ad0a
JoshAlexis <joshyelc@gmail.com>
2023-07-02 00:03:17 -0600

    feat(users): add domain errors
    - Added an interceptor to
    map domain error to http error.

--- src/users/application/get-user-by-email.use-case.ts
+++ src/users/application/get-user-by-email.use-case.ts
@@ -3,12 +3,17 @@ import { UsersTokens } from 'users/di/users.tokens'
 import { UsersRepository } from 'users/domain/users.repository'
 import { GetUserByEmailInputDto } from 'users/application/dto/get-user-by-email-input.dto'
 import { UserEntity } from 'users/domain/user.entity'
+import { UserNotFoundError } from 'users/domain/user-not-found.error'
 
 @Injectable()
 export class GetUserByEmail {
 	constructor(@Inject(UsersTokens.USER_REPOSITORY) private readonly repository: UsersRepository) {}
 
-	run(data: GetUserByEmailInputDto): Promise<UserEntity> {
-		return this.repository.findUserByEmail(data.email)
+	async run(data: GetUserByEmailInputDto): Promise<UserEntity> {
+		const user = await this.repository.findUserByEmail(data.email)
+
+		if (user === null) throw new UserNotFoundError()
+
+		return user
 	}
 }
